06cfdbdf93c99d034fbe680589f6511a6d01f3f3
mterrera29 <matiasterrera29@gmail.com>
2023-03-22 19:54:42 -0300

    nivel 1 responsive

--- src/components/context/PuntosContext.jsx
+++ src/components/context/PuntosContext.jsx
@@ -1,13 +1,13 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState } from 'react';
 
-export const PuntosContext = createContext(null)
+export const PuntosContext = createContext(null);
 
 export const PuntosProvider = ({ children }) => {
-  const [puntosTotal, setPuntosTotal] = useState(0)
+  const [puntosTotal, setPuntosTotal] = useState(0);
 
   return (
     <PuntosContext.Provider value={[puntosTotal, setPuntosTotal]}>
-        {children}
+      {children}
     </PuntosContext.Provider>
-  )
-}
+  );
+};
