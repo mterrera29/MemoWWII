8802eb8df239581d54a90d924f26859e7b735ef6
mterrera29 <matiasterrera29@gmail.com>
2023-03-22 19:09:24 -0300

    nivel 1 responsive

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
   return (
     <PuntosProvider>
       <BrowserRouter>
+        <MemoMain />
         <div className='App'>
           <Routes>
             <Route exact element={<MemoMain />} path='/' />
